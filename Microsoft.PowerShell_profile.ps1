5f34026719265e052207b22376e88b76a48fb842
sykuang <sykuang.tw@gmail.com>
2023-03-09 10:24:16 +0800

    Update install.ps1 and change theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_modern.omp.json" | Invoke-Expression
 
 Import-Module PSReadLine
 
--- install.ps1
+++ install.ps1
@@ -1,4 +1,16 @@
 $SCRIPT_PATH = $MyInvocation.MyCommand.Path
 $SCRIPT_FOLDER = Split-Path $SCRIPT_PATH -Parent
 
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target $SCRIPT_FOLDER\Microsoft.PowerShell_profile.ps1
+Write-Output "Installing windows tools"
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install fzf
+winget install sharkdp.fd
+winget install gerardog.gsudo
+BurntSushi.ripgrep.MSVC
+Install-Module -Name PSFzf
+
+if (-not(Test-Path -Path $PROFILE -PathType Leaf)){
+  New-Item -ItemType SymbolicLink -Path $PROFILE -Target $SCRIPT_FOLDER\Microsoft.PowerShell_profile.ps1
+}
+
+Write-Output "Done"

b435633bafbe2d5a128605991f5258b6c0ade057
sykuang <sykuang.tw@gmail.com>
2023-06-14 14:55:04 +0800

    feat(plugin): Add bat and pins to install script

--- install.ps1
+++ install.ps1
@@ -7,7 +7,9 @@ winget install fzf
 winget install sharkdp.fd
 winget install gerardog.gsudo
 winget install BurntSushi.ripgrep.MSVC
+winget install sharkdp.bat
 Install-Module -Name PSFzf
+Install-Module -Name pins
 
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)){
   New-Item -ItemType SymbolicLink -Path $PROFILE -Target $SCRIPT_FOLDER\Microsoft.PowerShell_profile.ps1
